49c6936a7c8ebb230193990af210fa184f1e1f42
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-05-23 22:26:45 +0900

    [33기 김형겸] ADD: Product 기본 UI 설정

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Product.scss';
 
 const Product = () => {
-  return <div>Product</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('data/productData.json')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+      });
+  }, []);
+
+  return (
+    <section className="luluProduct">
+      <div className="productWrapper">
+        <div className="productLeft">
+          <div className="productLeftName">
+            <h1>Men's Socks</h1>
+          </div>
+          <div>
+            <div className="filterBox">
+              <h2> Size </h2>
+              <button className="sizeButton">XS</button>
+              <button className="sizeButton">S</button>
+              <button className="sizeButton">M</button>
+              <button className="sizeButton">L</button>
+              <button className="sizeButton">XL</button>
+              <button className="sizeButton">XXL</button>
+            </div>
+            <div className="filterBox">
+              <h2> Color </h2>
+            </div>
+            <div className="filterBox">
+              <h2> Activity </h2>
+              <div className="activityOne">
+                <input type="checkbox" /> <span>Casual</span>
+              </div>
+              <div className="activityOne">
+                <input type="checkbox" /> <span>Casual</span>
+              </div>
+              <div className="activityOne">
+                <input type="checkbox" /> <span>Casual</span>
+              </div>
+              <div className="activityOne">
+                <input type="checkbox" /> <span>Casual</span>
+              </div>
+              <div className="activityOne">
+                <input type="checkbox" /> <span>Casual</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="productRight">
+          {products.map(product => {
+            const { id, src, name, price } = product;
+            return (
+              <div key={id}>
+                <img src={src} className="productImg" alt="product Image" />
+                <div className="productName">
+                  <span className="productId">{name}</span>
+                  <p>{price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Product;
