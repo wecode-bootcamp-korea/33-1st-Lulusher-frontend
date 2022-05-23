bc28031aa22cdfb326fc4867dbf4d7de11fa0b5c
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-05-23 17:58:21 +0900

    [Luluisher] Modify: eslint Feedback 수정, 컴포넌트 별 folder, scss 파일 생성

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './components/Main';
+import Bag from './pages/Bag/Bag';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Product from './pages/Product/Product';
+import ProductDetail from './pages/Product/ProductDetail/ProductDetail';
+import SignIn from './pages/SignIn/SignIn';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/bag" element={<Bag />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productdetail" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
