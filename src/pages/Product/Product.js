483572c061b320c673dbb959aee4082609765e89
Hyeong kyeom Kim <78855917+Kyeom1997@users.noreply.github.com>
2022-05-23 20:50:54 +0900

    [LuluIsher] Add: FE Coding Convention 정의

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Product = () => {
-  return <;
+  return;
+  <div>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>;
 };
 
 export default Product;
