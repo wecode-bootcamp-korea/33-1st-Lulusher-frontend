a10c1bcbd6ae29f7cca9c223b177b49cc326031d
Sorin0404 <haewong27@gmail.com>
2022-05-28 11:21:14 +0900

    for checkout

--- src/pages/Main/FirstSlide.js
+++ src/pages/Main/FirstSlide.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SlideCard from './SlideCard';
+import './FirstSlide.scss';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const FirstSlide = () => {
-  return <div>FirstSlide</div>;
+  const [sources, setSources] = useState([]);
+
+  useEffect(() => {
+    fetch('data/womanProductCarousel.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setSources(data);
+      });
+  }, []);
+
+  return (
+    <div className="FirstSlide">
+      <button type="button" className="carouselArrowLeft">
+        <FaArrowLeft />
+      </button>
+      {sources.map((source, index) => (
+        <SlideCard key={index} source={source} />
+      ))}
+      <button type="button" className="carouselArrowRight">
+        <FaArrowRight />
+      </button>
+    </div>
+  );
 };
 
 export default FirstSlide;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,67 +1,73 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 import './Main.scss';
+import FirstSlide from './FirstSlide';
 
 const Main = () => {
   return (
-    <div className="Main">
-      <Navbar />
-      <div className="firstMain">
-        <img
-          className="firstPosition"
-          src="https://images.lululemon.com/is/image/lululemon/na_may22_wk4_W_Summer_3_1_D_HP?wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
-          alt="woman summer"
-        />
-        <div className="Writing">
-          <div className="firstWriting">Turn up summer.</div>
-          <div className="secondWriting">
-            <p>Mix light and breathable. Match every move under the sun.</p>
+    <>
+      <div className="Main">
+        <Navbar />
+        <div className="firstMain">
+          <img
+            className="firstPosition"
+            src="https://images.lululemon.com/is/image/lululemon/na_may22_wk4_W_Summer_3_1_D_HP?wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
+            alt="woman summer"
+          />
+          <div className="Writing">
+            <div className="firstWriting">Turn up summer.</div>
+            <div className="secondWriting">
+              <p>Mix light and breathable. Match every move under the sun.</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="firstMain">
-        <img
-          className="firstPosition"
-          src="https://images.lululemon.com/is/image/lululemon/na_may22_wk4_M_Swim_3_1_D_Static?wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
-          alt="man summer"
-        />
-        <div className="Writing">
-          <div className="firstWriting">Freestyle your summer.</div>
-          <div className="secondWriting">
-            <p>
-              Beaches, parks, or pools. In breathable swim shorts, just plan on
-              having fun.
-            </p>
+        <div className="firstMain">
+          <img
+            className="firstPosition"
+            src="https://images.lululemon.com/is/image/lululemon/na_may22_wk4_M_Swim_3_1_D_Static?wid=2420&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
+            alt="man summer"
+          />
+          <div className="Writing">
+            <div className="firstWriting">Freestyle your summer.</div>
+            <div className="secondWriting">
+              <p>
+                Beaches, parks, or pools. In breathable swim shorts, just plan
+                on having fun.
+              </p>
+            </div>
           </div>
         </div>
+        <div className="goToCategorySite">
+          <div className="pushSite">
+            <img
+              src="https://images.lululemon.com/is/image/lululemon/na_may22_wk3_W_Shorts_1_3_Md_MediaAction_D_WorkoutShorts?wid=750&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
+              alt="push"
+            />
+            <p>Push.</p>
+            <button type="button">SHOP WORKOUT SHORTS</button>
+          </div>
+          <div className="poseSite">
+            <img
+              src="https://images.lululemon.com/is/image/lululemon/na_may22_wk3_W_Shorts_1_3_Md_MediaAction_D_YogaShorts?wid=750&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
+              alt="pose"
+            />
+            <p>Pose.</p>
+            <button type="button">SHOP WORKOUT SHORTS</button>
+          </div>
+          <div className="paceSite">
+            <img
+              src="https://images.lululemon.com/is/image/lululemon/na_may22_wk3_W_Shorts_1_3_Md_MediaAction_D_RunningShorts?wid=750&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
+              alt="Pace"
+            />
+            <p>Pace.</p>
+            <button type="button">SHOP WORKOUT SHORTS</button>
+          </div>
+        </div>
+        <FirstSlide />
       </div>
-      <div className="goToCategorySite">
-        <div className="pushSite">
-          <img
-            src="https://images.lululemon.com/is/image/lululemon/na_may22_wk3_W_Shorts_1_3_Md_MediaAction_D_WorkoutShorts?wid=750&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
-            alt="push"
-          />
-          <p>Push.</p>
-          <button type="button">SHOP WORKOUT SHORTS</button>
-        </div>
-        <div className="poseSite">
-          <img
-            src="https://images.lululemon.com/is/image/lululemon/na_may22_wk3_W_Shorts_1_3_Md_MediaAction_D_YogaShorts?wid=750&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
-            alt="pose"
-          />
-          <p>Pose.</p>
-          <button type="button">SHOP WORKOUT SHORTS</button>
-        </div>
-        <div className="paceSite">
-          <img
-            src="https://images.lululemon.com/is/image/lululemon/na_may22_wk3_W_Shorts_1_3_Md_MediaAction_D_RunningShorts?wid=750&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"
-            alt="Pace"
-          />
-          <p>Pace.</p>
-          <button type="button">SHOP WORKOUT SHORTS</button>
-        </div>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
