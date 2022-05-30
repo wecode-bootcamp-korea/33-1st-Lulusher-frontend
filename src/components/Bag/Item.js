d8d92f1b833bb449f9e7473062b86a664575c7b7
Rey <sooyeon.rok@gmail.com>
2022-05-30 20:36:40 +0900

    Modify: 회원가입 모달창 구현
    inline style과 useState로 구현했던 회원가입 모달창을 삼항연산자를 활용해서 modal변수의 값에 따라 다른 컴포넌트를 보여주는 것으로 수정했습니다

--- src/components/Bag/Item.js
+++ src/components/Bag/Item.js
@@ -1,51 +1,7 @@
 import React from 'react';
 
 const Item = () => {
-  return (
-    <div className="productItem">
-      <img src="https://images.lululemon.com/is/image/lululemon/LW7AWBT_045610_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72" />
-      <div className="productWrapper">
-        <div className="productDescription">
-          <h2>Product Name</h2>
-          <p>Product Color</p>
-          <p>Product Size</p>
-        </div>
-        <div className="productOptions">
-          <table>
-            <thead>
-              <tr>
-                <th>Item Price</th>
-                <th>Quantity</th>
-                <th>Total Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>$price</td>
-                <td>
-                  <select>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </select>
-                </td>
-                <td>$price</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="productFooter">
-          <p>Free Shipping + Free Returns</p>
-          <div className="footerBtns">
-            <button>Save for Later</button>
-            <button>Remove</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="productItem" />;
 };
 
 export default Item;
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -33,9 +33,7 @@ const SignIn = () => {
 
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
-  const [modal, setModal] = useState(false);
-  const [modalBtnStyle, setModalBtnStyle] = useState({ display: 'block' });
-  const [signInStyle, setSignInStyle] = useState({ display: 'block' });
+  const [modal, setModal] = useState(true);
 
   const isValidEmail = id.includes('@') && id.includes('.');
   const isValidPassword = pw.length >= 8;
@@ -76,56 +74,56 @@ const SignIn = () => {
             </ul>
           </div>
           <div className="rightContainer">
-            <div className="signInContainer" style={signInStyle}>
-              <h1>Sign in to your account</h1>
-              <div className="borderBottom" />
-              <div className="signInWrapper">
-                <form>
-                  <label>
-                    Email address
-                    <input
-                      type="text"
-                      onChange={handleIdInput}
-                      value={id}
-                      required
-                    />
-                  </label>
-                  <label>
-                    Password
-                    <input
-                      type="password"
-                      onChange={handlePwInput}
-                      value={pw}
-                      required
-                    />
-                  </label>
-                  <a href="#!">Forgot your password?</a>
-                  <button
-                    className={
-                      isValidSignIn
-                        ? 'signInBtnActivated'
-                        : 'signInBtnDeActivated'
-                    }
-                    onClick={goToMain}
-                  >
-                    SIGN IN
-                  </button>
-                </form>
-                <hr />
+            {modal ? (
+              <div className="signInContainer">
+                <h1>Sign in to your account</h1>
+                <div className="borderBottom" />
+                <div className="signInWrapper">
+                  <form>
+                    <label>
+                      Email address
+                      <input
+                        type="text"
+                        onChange={handleIdInput}
+                        value={id}
+                        required
+                      />
+                    </label>
+                    <label>
+                      Password
+                      <input
+                        type="password"
+                        onChange={handlePwInput}
+                        value={pw}
+                        required
+                      />
+                    </label>
+                    <a href="#!">Forgot your password?</a>
+                    <button
+                      className={
+                        isValidSignIn
+                          ? 'signInBtnActivated'
+                          : 'signInBtnDeActivated'
+                      }
+                      onClick={goToMain}
+                    >
+                      SIGN IN
+                    </button>
+                  </form>
+                  <hr />
+                </div>
+                <div
+                  className="clickToRegister"
+                  onClick={() => {
+                    handleModal();
+                  }}
+                >
+                  <h1>Create a luluisher account</h1>
+                </div>
               </div>
-            </div>
-            {modal === true ? <SignUp /> : null}
-            <div
-              className="clickToRegister"
-              style={modalBtnStyle}
-              onClick={() => {
-                handleModal();
-                setModalBtnStyle({ display: 'none' });
-                setSignInStyle({ display: 'none' });
-              }}
-            >
-              <h1>Create a luluisher account</h1>
-            </div>
+            ) : (
+              <SignUp />
+            )}
           </div>
         </div>
       </div>
