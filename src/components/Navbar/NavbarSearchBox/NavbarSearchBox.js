ea9b3e0a5dfc601ddf6e108dfdc8b0f63bb9e78f
Sorin0404 <haewong27@gmail.com>
2022-06-02 17:10:10 +0900

    update Navbar

--- src/components/Navbar/NavbarSearchBox/NavbarSearchBox.js
+++ src/components/Navbar/NavbarSearchBox/NavbarSearchBox.js
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from 'react';
-// import { Navigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './NavbarSearchBox.scss';
 
 const NavbarSearchBox = () => {
   const [products, setProducts] = useState([]);
   const [productInput, setProductInput] = useState('');
-
-  // useEffect(() => {
-  //   fetch(`http://10.58.0.59:8000/products/list`, {
-  //     method: 'GET',
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setProducts(data);
-  //     });
-  // }, []);
 
   const updateProductInput = e => {
     setProductInput(e.target.value);
@@ -25,19 +15,21 @@ const NavbarSearchBox = () => {
   });
 
   return (
-    <form className="navbarSearchBox" handlechange={updateProductInput}>
-      <img
-        className="magnifyingGlass"
-        src="/images/Navbar/magnifying-glass.png"
-        alt="magnifying-glass"
-      />
-      <input
-        type="text"
-        className="searchBar"
-        placeholder="Search"
-        onChange={sortedProducts}
-      />
-    </form>
+    <Link to="/">
+      <form className="navbarSearchBox" handlechange={sortedProducts}>
+        <img
+          className="magnifyingGlass"
+          src="/images/Navbar/magnifying-glass.png"
+          alt="magnifying-glass"
+        />
+        <input
+          type="text"
+          className="searchBar"
+          placeholder="Search"
+          onChange={updateProductInput}
+        />
+      </form>
+    </Link>
   );
 };
 
