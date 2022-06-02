39fb957165017604dd0a3d7f8a9dbe3e1133cf35
Rey <sooyeon.rok@gmail.com>
2022-06-02 09:50:36 +0900

    git checkout

--- src/components/Bag/Item.js
+++ src/components/Bag/Item.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Item.scss';
 
 const Item = ({ item, itemList, onRemove, isEmpty, setEmpty }) => {
